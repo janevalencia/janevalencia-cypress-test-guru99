0f7e04ecc4ad6ad64d1032e4d20b2192f4e17181
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-18 18:38:42 +0800

    cypress e2e testing init

---
+++ cypress/e2e/test_spec.cy.js
@@ -0,0 +1,12 @@
+/**
+ * Author: Jane Valencia
+ * Version: 1.0.0
+ * 
+ * This is just to be used as a test run,
+ * to verify that Cypress is connected and working.
+ */
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
