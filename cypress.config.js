7cc18549a72caf347401736e57fed71c2e73fddd
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-19 02:27:44 +0800

    Setup cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // Global config unless overridden in test suite or individual test.
+  video: true,
+  videoCompression: 32,
+  retries: {
+    runMode: 0,
+    openMode: 0,
+  },
+  // Config for E2E tests.
   e2e: {
+    baseUrl: 'https://demo.guru99.com/V4/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
