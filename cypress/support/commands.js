e29cb7dc2c69b86d844658a27c06e9547fb8b62a
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-19 02:28:12 +0800

    Create custom command typeValidEmailToSelector

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('typeValidEmailToSelector', (selector) => {
+    function createEmail() {
+        const prefix = 'test';
+        const domain = 'email.ghostinspector.com'
+        let timestampms = Date.now();
+        return `${prefix}_${timestampms}@${domain}`;
+    }
+    cy.get(selector).type(createEmail());
+})
