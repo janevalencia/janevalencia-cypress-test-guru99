659ed079159384a3810f6dc8457631c643630911
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-19 15:05:45 +0800

    Clean up commands.js and add comment

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
+// Helper custom command to create random valid email and enter into an input selector.
 Cypress.Commands.add('typeValidEmailToSelector', (selector) => {
     function createEmail() {
         const prefix = 'test';
