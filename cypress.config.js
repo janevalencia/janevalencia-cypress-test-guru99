ccbc7aac46c30d5c93d02bafacb077266b413f42
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-21 23:57:56 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   },
   // Config for E2E tests.
   e2e: {
-    baseUrl: 'https://demo.guru99.com/V4',
+    baseUrl: 'https://demo.guru99.com/V4/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
