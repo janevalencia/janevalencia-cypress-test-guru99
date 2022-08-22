0a8b4aaea462b72750947ae3d513ed2e51a95ce7
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-22 17:04:45 +0800

    Re-arrange and add more tests about input length

--- cypress/e2e/customer/guru99-add-customer_spec.cy.js
+++ cypress/e2e/customer/guru99-add-customer_spec.cy.js
@@ -2,21 +2,30 @@ import { Signin, AddCustomer } from "../../pages";
 const signin = new Signin();
 const addCustomer = new AddCustomer();
 
-describe('TS_3a: Verify Add Customer function', () => {
+describe('TS_3a: Verify Add New Customer Form Submission', () => {
     beforeEach('Login and navigate to Add Customer page', () => {
+        // Go to login page.
         signin.start();
+
+        // Login as manager.
         cy.fixture('login').then((user) => {
             signin.signin(user.userID, user.password)
         })
+
+        // Navigate to New Customer page.
         addCustomer.navigate();
     })
 
-    // Ignore all uncaught exception by the application itself.
+    // Ignore all uncaught exception thrown by the application.
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
     });
 
-    it('User should be able to add new customer with all valid information entered', () => {
+    it('User should be able to see the Add New Customer form.', () => {
+        addCustomer.validateAddCustomerPage();
+    })
+
+    it('User should be able to submit Add New Customer form with all required information.', () => {
         cy.fixture('customer').then((customer) => {
             
             // Enter valid customer data.
@@ -32,44 +41,49 @@ describe('TS_3a: Verify Add Customer function', () => {
             addCustomer.inputPassword(customer.password);
 
             // Execute form submission.
-            addCustomer.submit();
+            addCustomer.runSubmit();
 
             // Validate newly created customer data.
             addCustomer.validateSubmittedCustomer();
         })
     })
+
+    it('User should not be able to submit Add New Customer form without all required fields.', () => {
+        addCustomer.runInvalidSubmission('please fill all fields');
+    })
 })
 
-describe('TS_3b: Verify Add Customer UI Form', () => {
+describe('TS_3b: Verify Add New Customer UI Form', () => {
     before('Login and navigate to Add Customer page', () => {
+        // Go to login page.
         signin.start();
+
+        // Login as manager.
         cy.fixture('login').then((user) => {
             signin.signin(user.userID, user.password)
         })
-        addCustomer.navigate();
-    })
 
-    it('User should be able to see the Add New Customer form.', () => {
-        addCustomer.validateAddCustomerPage();
+        // Navigate to New Customer page.
+        addCustomer.navigate();
     })
 
     it('User should see input error message if customer name is invalid.', () => {
         addCustomer.validateEmptyCustomerName('Customer name must not be blank');
         addCustomer.validateInvalidCustomerName(' start with space', 'First character can not have space');
+    })
+
+    it('User should not be able to enter over 25 characters of customer name', () => {
         cy.fixture('customer').then((customer) => {
             addCustomer.validateCustomerNameLength(customer.nameOver25Chars)
         })
     })
 
-    it('User should be able to change the default M gender.', () => {
-        // Select 'female' for gender.
+    it('User should be able to select gender.', () => {
         addCustomer.inputGender(1);
-
-        // Select 'male' for gender.
         addCustomer.inputGender(0);
     })
 
-    it('User should only able to enter valid date into DOB input field.', () => {
+    it('User should only be able to enter valid date (YYYY-MM-DD) into DOB input field.', () => {
         cy.fixture('customer').then((customer) => {
             addCustomer.validateValidDOB(customer.dob, 'Date Field must not be blank');
         })
@@ -78,65 +92,74 @@ describe('TS_3b: Verify Add Customer UI Form', () => {
     it('User should see input error message if address is invalid.', () => {
         addCustomer.validateEmptyAddress('Address Field must not be blank');
         addCustomer.validateInvalidAddress(' start with space', 'First character can not have space');
-        // length test case
+    })
+
+    it('User should not be able to enter over 50 characters of street address', () => {
+        cy.fixture('customer').then((customer) => {
+            addCustomer.validateAddressLength(customer.address.streetOver50len);
+        })
     })
 
     it('User should see input error message if city is invalid.', () => {
         addCustomer.validateEmptyCity('City Field must not be blank');
         addCustomer.validateInvalidCity(' start with space', 'First character can not have space');
-        // length test case
+    })
+
+    it('User should not be able to enter over 25 characters of city address', () => {
+        cy.fixture('customer').then((customer) => {
+            addCustomer.validateCityLength(customer.address.cityOver25len);
+        })
     })
 
     it('User should see input error message if state is invalid.', () => {
         addCustomer.validateEmptyState('State must not be blank');
         addCustomer.validateInvalidState(' start with space', 'First character can not have space');
-        // length test case
+    })
+
+    it('User should not be able to enter over 25 characters of state address', () => {
+        cy.fixture('customer').then((customer) => {
+            addCustomer.validateStateLength(customer.address.stateOver25len);
+        })
     })
 
     it('User should see input error message if PIN number is invalid.', () => {
         addCustomer.validateEmptyPIN('PIN Code must not be blank');
         addCustomer.validateInvalidPIN(' ', 'First character can not have space');
-
-        // Clear the form.
-        cy.get('input[type="reset"]').click();
-
         addCustomer.validateInvalidPIN('text', 'Characters are not allowed');
-
-        // Clear the form.
-        cy.get('input[type="reset"]').click();
-
         addCustomer.validateInvalidPIN('1234', 'PIN Code must have 6 Digits');
-        
-        // length test case
+    })
+
+    it('User should not be able to enter over 6 characters of PIN', () => {
+        cy.fixture('customer').then((customer) => {
+            addCustomer.validatePINLength(customer.pinOver6len);
+        })
     })
 
     it('User should see input error message if mobile number is invalid.', () => {
-        // Validate empty mobile number.
         addCustomer.validateEmptyMobile('Mobile no must not be blank');
-        
-        // Validate enter space into mobile number.
         addCustomer.validateInvalidMobile(' space', 'First character can not have space');
-        
-        // Clear the form.
-        cy.get('input[type="reset"]').click();
-        
-        // Validate entering string into mobile number.
         addCustomer.validateInvalidMobile('text', 'Characters are not allowed');
-        
-        // length test case
+    })
+
+    it('User should not be able to enter over 15 characters of mobile number', () => {
+        cy.fixture('customer').then((customer) => {
+            addCustomer.validateMobileLength(customer.mobileOver15len);
+        })
     })
 
     it('User should see input error message if email is invalid.', () => {
         addCustomer.validateEmptyEmail('Email-ID must not be blank');
         addCustomer.validateInvalidEmail(' startwithspace', 'First character can not have space');
-        // length test case
+        addCustomer.validateInvalidEmail('test@awd.c', 'Email-ID is not valid');
+    })
+
+    it('User should not be able to enter over 30 characters of email', () => {
+        cy.fixture('customer').then((customer) => {
+            addCustomer.validateEmailLength(customer.emailOver30len);
+        })
     })
 
     it('User should see input error message if password is empty.', () => {
         addCustomer.validateEmptyPassword('Password must not be blank');
-    })
-
-    it('User should not be able to submit new customer without filling all required fields.', () => {
-        addCustomer.invalidSubmission('please fill all fields');
     })
 })
