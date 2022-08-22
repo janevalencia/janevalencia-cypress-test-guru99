6c4d8821612cb052ee37902b1a7a87d684d5052a
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-08-22 19:36:03 +0800

    POM: Signup, add construct to define element selectors

--- cypress/pages/Signup.js
+++ cypress/pages/Signup.js
@@ -1,24 +1,41 @@
 class Signup {
-    // Go to signup URL.
+
+    /**
+     * Define form elements (wrapped in function).
+     */
+    constructor() {
+        this.emailID = () => cy.get(`input[name="emailid"]`);
+        this.signUpBtn = () => cy.get(`input[type="submit"]`);
+        this.accountCreated = () => cy.get('.accpage');
+    }
+
+    /**
+     * Go to Sign Up page.
+     */
     start() {
         cy.visit('https://demo.guru99.com/');
     }
 
-    // Execute signup action.
+    /**
+     * Execute signup - creating new login account.
+     */
     signup() {
         // Enter valid email using custom Cypress command (/support/commands.js) to create random valid email.
         cy.typeValidEmailToSelector(`input[name="emailid"]`);
 
         // Click signup button.
-        cy.get(`input[type="submit"]`).click();
-
-        return this;
+        this.signUpBtn().click();
     }
 
-    // Validate submitting invalid empty email to signup form.
+    /**
+     * Validate submitting invalid empty email to signup form.
+     * 
+     * @param {String} expectedInputErrMsg 
+     * @returns Cypress assertion command chain.
+     */
     validateEmptyEmail(expectedInputErrMsg) {
         // Click signup button.
-        cy.get(`input[type="submit"]`).click();
+        this.signUpBtn().click();
 
         // Check for invalid input message prompt.
         cy.get('#message9').should('have.text', expectedInputErrMsg);
@@ -26,13 +43,19 @@ class Signup {
         return this;
     }
 
-    // Validate submitting invalid email address to signup form.
+    /**
+     * Validate submitting invalid email address to signup form.
+     * 
+     * @param {String} invalidEmail 
+     * @param {String} expectedInputErrMsg 
+     * @returns Cypress assertion command chain.
+     */
     validateInvalidEmail(invalidEmail, expectedInputErrMsg) {
         // Enter empty email.
-        cy.get(`input[name="emailid"]`).type(invalidEmail);
+        this.emailID().type(invalidEmail);
 
         // Click signup button.
-        cy.get(`input[type="submit"]`).click();
+        this.signUpBtn().click();
 
         // Check for invalid input message prompt.
         cy.get('#message9').should('have.text', expectedInputErrMsg);
@@ -40,33 +63,33 @@ class Signup {
         return this;
     }
 
-    // Validate successfully signup should return temporary id and password.
+    /**
+     * Validate successfully signup should return temporary id and password.
+     */
     validateSuccessSignup() {
         // Check for generated user id should be visible.
-        cy.get('.accpage').eq(0).siblings().should('be.visible');
+        this.accountCreated().eq(0).siblings().should('be.visible');
 
         // Check for generated password should be visible.
-        cy.get('.accpage').eq(1).siblings().should('be.visible');
+        this.accountCreated().eq(1).siblings().should('be.visible');
 
         // Check for the warning message of account validity.
         cy.contains('valid only for 20 days').should('be.visible');
-
-        return this;
     }
 
-    // Validate signup page UI elements upon visiting.
+    /**
+     * Validate signup page UI elements upon visiting.
+     */
     validateSignupPage() {
         // Check email input form.
-        cy.get(`input[name="emailid"]`)
+        this.emailID()
             .should('exist')
             .should('be.visible');
         
         // Check for the submit button.
-        cy.get(`input[type="submit"]`)
+        this.signUpBtn()
             .should('be.visible')
             .should('be.enabled');
-
-        return this;
     }
 }
 
